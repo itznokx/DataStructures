264fb0694bb0a810faf21b388c491010410d92b3
Dimitri Medeiros <79457494+itznokx@users.noreply.github.com>
2024-06-28 14:12:50 -0300

    Sort method added
    linkedlist sort method using merge sort

--- linkedlist/list.cpp
+++ linkedlist/list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 struct ListIndex {
 	ListIndex(int v) : value(v),next(nullptr) {}
 	ListIndex* next;
@@ -18,6 +19,10 @@ struct List
 			std::cout << aux->value << " ";
 			aux = aux->next;
 		}
+		std::cout << "\n";
+	}
+	int getSize(){
+		return this->size;
 	}
 	//add to end
 	void add(int v){
@@ -86,10 +91,75 @@ struct List
 			}
 		}
 	}
-	LinkedIndex* getTail (){
+	ListIndex* getIndex(int pos){
+		if ((pos>=this->size)){
+			std::cout << "Invalid Position.\n";
+			return nullptr;
+		}else{
+			ListIndex* aux = this->first;
+			for (int i=0;i<this->size;i++){
+				if (i == pos)
+					return (aux);
+				aux = aux->next;
+			}
+		}
+		return nullptr;
+	}
+	void sort(){
+		this->mergeSort(this);
+	}
+	void mergeSort(List *head){
+		
+			ListIndex* headRef = head->first;
+			List* left = new List();
+			List* right = new List();
+			if (headRef == nullptr || headRef->next == nullptr){
+				return;
+			}
+			splitList(headRef,left,right);
+			mergeSort(left);
+			mergeSort(right);
+			head->first = sort2Lists(left->first,right->first);
+	}
+	void splitList (ListIndex* headRef,List* left,List* right){
+		
+		ListIndex* jumper;
+		ListIndex* doubleJumber;
+		jumper = headRef;
+		doubleJumber = headRef->next;
+		while (doubleJumber!=nullptr){
+			doubleJumber = doubleJumber->next;
+			if (doubleJumber!=nullptr){
+				jumper = jumper->next;
+				doubleJumber = doubleJumber->next;
+			}
+		}
+
+		left->first = headRef;
+		right->first = jumper->next;
+		jumper->next = nullptr;
+	}
+	ListIndex* sort2Lists(ListIndex* left, ListIndex* right){
+		ListIndex* aux = nullptr;
+		if (left==nullptr)
+			return right;
+		else if (right==nullptr)
+			return left;
+		if (left->value <= right->value){
+			aux = left;
+			aux->next = sort2Lists(left->next,right);
+
+		}else {
+			aux = right;
+			aux->next = sort2Lists(left,right->next);
+		}
+		return aux;
+
+	}
+	ListIndex* getTail (){
 		if (this->first==nullptr)
-			return nullptr
-		LinkedIndex* aux = this->first;
+			return nullptr;
+		ListIndex* aux = this->first;
 		while(aux->next != nullptr){
 			aux = aux->next;
 		}
